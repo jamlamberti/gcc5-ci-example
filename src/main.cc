d2ce1c1097db52ed435e6ba1ad533940b5f82f51
Jack Lamberti <jal416@lehigh.edu>
2016-04-10 23:10:32 -0400

    Added a simple test case

--- src/main.cc
+++ src/main.cc
@@ -1,6 +1,12 @@
 #include <iostream>
 #include <gtest/gtest.h>
 
+TEST(SomeTestClass, SpecificTest)
+{
+    EXPECT_EQ(3+3, 6);
+    EXPECT_NE(2+2, 5);
+}
+
 int main(int argc, char** argv)
 {
     #ifdef TESTING
